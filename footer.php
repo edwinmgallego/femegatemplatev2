56caaa1a2b86ae463f58d6af0a6fbd8ae726380e
edwinmgallego <ing.edwin.gallego@gmail.com>
2022-02-14 10:04:33 -0500

    arreglando  functions.php y  el footer.php

--- footer.php
+++ footer.php
@@ -1,16 +1,22 @@
+    
+       <div class="col-md-4 modulo-sidebar">
+            <?php if(is_active_sidebar('sidebar')) : ?>
+                <?php dynamic_sidebar('sidebar'); ?>
+            <?php endif;?>
+          </div>
+
+      </div><!-- /.row -->
+
+    </div><!-- /.container -->
+
 
    
+     <footer class="blog-footer">
+      <p><?php bloginfo('name'); ?></p>
+       <p><?php bloginfo('description'); ?></p>
+    </footer>    
 
-</div><!-- /.container -->
-
- <footer class="blog-footer">
-  <p><?php bloginfo( 'name' );?></p>
-  <p>
-    <a href="#"><?php bloginfo( 'description' );?></a>
-  </p>
-</footer>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="js/bootstrap.js"></script>
-</body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.js"></script>
+  </body>
 </html>
--- functions.php
+++ functions.php
@@ -1,20 +1,24 @@
 <?php 
-//inportacion del  navwalker para  menu
-require_once('wp-bootstrap-navwalker.php');
-//sopoerte  para  este  tema
+	
+	//Importacion navwalker para menu
+	require_once('wp-bootstrap-navwalker.php');
 
-function wpb_theme_setup(){
+	//Soporte para este tema
+	function wpb_theme_setup(){
+		add_theme_support('post-thumbnails');
 
-    add_theme_support('post-thumbnails');//soporte  para  imagenes del tema
-    //menus  de  navegacion
-    register_nav_menu( array(
-        'primary'=>__('Primary Menu')
-    ));
-   //formatos  de post
-   add_theme_support('post-formats',array('aside','gallery'));
 
-}
-add_action('after_setup_theme','wpb_theme_setup');
+		//Menús de navegación
+		register_nav_menus(array(
+			'primary' => __('Primary Menu')
+		));
+
+
+		//Soporte para formatos de post
+		add_theme_support('post-formats', array('aside', 'gallery'));
+	}
+
+	add_action('after_setup_theme', 'wpb_theme_setup');
 
 
 
